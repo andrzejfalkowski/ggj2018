fa4228af9c27a31089baf70f5f2b01822261f2c2
Andrzej Fałkowski <vkowsky@gmail.com>
2018-01-26 21:06:07 +0100

    Dotween, camera controls, simple game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager Instance = null;
+
+	public enum EGameState
+	{
+		GAME_STARTING,
+		GAME_IN_PROGRESS,
+		GAME_OVER
+	}
+
+	public EGameState CurrentGameState = EGameState.GAME_STARTING;
+
+	public void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+
+	}
+
+	void Update()
+	{
+		switch(CurrentGameState)
+		{
+			case EGameState.GAME_STARTING:
+				break;
+			case EGameState.GAME_IN_PROGRESS:
+				break;
+			case EGameState.GAME_OVER:
+				break;
+		}
+	}
+}

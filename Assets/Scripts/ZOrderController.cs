dda123e08c6a213b5dfac7d71c879e0d9f6060df
Andrzej Fałkowski <vkowsky@gmail.com>
2018-01-26 22:41:31 +0100

    -[Code] Dynamic Z order.

--- Assets/Scripts/ZOrderController.cs
+++ Assets/Scripts/ZOrderController.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZOrderController : MonoBehaviour {
+public class ZOrderController : MonoBehaviour 
+{
+    public const float GRANULARITY = 10f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    [SerializeField]
+    private GameObject positionSurce = null;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer = null;
+
+    [SerializeField]
+    private bool updateOnlyOnStart = false;
+
+    [SerializeField]
+    private float zShift = 0f;
+
+    void Start()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+        }
+
+        if (positionSurce == null)
+        {
+            positionSurce = this.gameObject;
+        }
+    }
+
+	void Update () 
+    {
+        if (!updateOnlyOnStart)
+        {
+            spriteRenderer.sortingOrder = (int)Mathf.Clamp(GRANULARITY * -positionSurce.transform.localPosition.y + GRANULARITY * zShift, 
+                short.MinValue, short.MaxValue);
+        }
 	}
 }

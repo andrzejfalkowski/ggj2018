4a3daa61b51019c31ea9e1416fb08b9d24274acd
Dominik <dominikpielak@gmail.com>
2018-01-28 13:13:01 +0100

    -[Code] Camera follows player's squad

--- Assets/Scripts/Input/CameraController.cs
+++ Assets/Scripts/Input/CameraController.cs
@@ -15,47 +15,57 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private float viewportBorderTreshold = 0.01f;
 
+    [SerializeField]
+    private bool followPlayerSquad = true;
+
     private bool rightClick = false;
     private Vector2 lastRightClickPos;
 
 	// Update is called once per frame
 	void Update () 
     {
-
-        if (Input.GetMouseButton(1))
+        if (!followPlayerSquad)
         {
-            if (!rightClick)
+            if (Input.GetMouseButton(1))
             {
-                rightClick = true;
+                if (!rightClick)
+                {
+                    rightClick = true;
+                }
+                else
+                {
+                    MoveBy((lastRightClickPos - (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+                }
+
+                lastRightClickPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                //MoveTo(new Vector3(mouseWorldPos.x, mouseWorldPos.y, this.transform.localPosition.z));
             }
             else
             {
-                MoveBy((lastRightClickPos - (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+                rightClick = false;
+
+                Vector2 mouseViewportPos = viewportBordersEnabled ?
+                    (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition) : new Vector2(0.5f, 0.5f);
+
+                bool left = Input.GetKey(KeyCode.LeftArrow) || mouseViewportPos.x < viewportBorderTreshold;
+                bool right = Input.GetKey(KeyCode.RightArrow) || mouseViewportPos.x > 1f - viewportBorderTreshold;
+                bool up = Input.GetKey(KeyCode.UpArrow) || mouseViewportPos.y > 1f - viewportBorderTreshold;
+                bool down = Input.GetKey(KeyCode.DownArrow) || mouseViewportPos.y < viewportBorderTreshold;
+
+                if (left || right || up || down)
+                {
+                    MoveBy(new Vector2(left ? -scrollStrength : (right ? scrollStrength : 0f),
+                            up ? scrollStrength : (down ? -scrollStrength : 0f)));
+                }
             }
-
-            lastRightClickPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            //MoveTo(new Vector3(mouseWorldPos.x, mouseWorldPos.y, this.transform.localPosition.z));
         }
         else
         {
-            rightClick = false;
-
-            Vector2 mouseViewportPos = viewportBordersEnabled ? 
-                (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition) : new Vector2(0.5f, 0.5f);
-
-            bool left = Input.GetKey(KeyCode.LeftArrow) || mouseViewportPos.x < viewportBorderTreshold;
-            bool right = Input.GetKey(KeyCode.RightArrow) || mouseViewportPos.x > 1f - viewportBorderTreshold;
-            bool up = Input.GetKey(KeyCode.UpArrow) || mouseViewportPos.y > 1f - viewportBorderTreshold;
-            bool down = Input.GetKey(KeyCode.DownArrow) || mouseViewportPos.y < viewportBorderTreshold;
-
-            if (left || right || up || down)
-            {
-                MoveBy(new Vector2(left ? -scrollStrength : (right ? scrollStrength : 0f), 
-                        up ? scrollStrength : (down ? -scrollStrength : 0f)));
-            }
+            Vector3 squadPos = GameManager.Instance.GetPlayerSquadPosition();
+            squadPos.z = transform.position.z;
+            MoveTo(squadPos);
         }
-	}
+    }
 
     void MoveBy(Vector2 shift)
     {
